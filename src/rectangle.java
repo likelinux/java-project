d9601ee633b6d4bf153f8eb3ecf83524ee6f9260
Singh, Manpreet <manpreet_singh@baxter.com>
2018-01-24 19:02:52 +0530

    Initial Java Commit

---
+++ src/rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+	 public int lenght;
+	 public int width;
+
+	 public Rectangle(int lenght, int width) {
+		 this.lenght = lenght;
+		 this.width =width;
+	 }
+
+
+	 public int getArea() {
+		 return lenght * width;
+	 }
+
+	 public int getPerimeter() {
+		 return 2 * (lenght + width);
+	 }
+
+}
