000d1ad3c82f31bd55dd475feaec54e9754557cf
Singh, Manpreet <manpreet_singh@baxter.com>
2018-01-25 14:45:48 +0530

    New code Changes

--- src/rectangle.java
+++ src/rectangle.java
@@ -1,19 +1,17 @@
 public class Rectangle {
-	 public int lenght;
-	 public int width;
+  public int length;
+  public int width;
 
-	 public Rectangle(int lenght, int width) {
-		 this.lenght = lenght;
-		 this.width = width;
-	 }
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
+  }
 
+  public int getArea() {
+    return length * width;
+  }
 
-	 public int getArea() {
-		 return lenght * width;
-	 }
-
-	 public int getPerimeter() {
-		 return 2 * (lenght + width);
-	 }
-
+  public int getPerimeter() {
+    return 2 * (length + width);
+  }
 }
