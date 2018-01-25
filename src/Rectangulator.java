9a3483d38bfe9693faf5588009b8c4fc4ccd2cab
Singh, Manpreet <manpreet_singh@baxter.com>
2018-01-25 14:24:43 +0530

    Changes in src folder

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,9 +1,9 @@
 public class Rectangulator {
 	public static void main(Sring[] args) {
-		int lenght = Integer.parseInt(args[0]);
+		int length = Integer.parseInt(args[0]);
 		int width = Integer.parseInt(args[1]);
 
-		Rectandle myRectangle = new Rectangle(length, width);
+		Rectangle myRectangle = new Rectangle(length, width);
 
 		String output = String.format("*** Your Rectanle ***\n\nLenght: %d\nWidth: %d\nPerimeter: %d\n\n", myRectangle.length, myRectangle.width, myRectangle.getArea(), myRectangle.getPerimeter());
 		System.out.println(output);
